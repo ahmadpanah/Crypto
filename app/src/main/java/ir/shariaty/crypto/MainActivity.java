a6811dd3b002a2c05a6280c4a1eadab24d333aa1
Hossein <djalicrt@gmail.com>
2021-12-13 12:08:53 +0330

    Finish Recycler View Item Layout

--- app/src/main/java/ir/shariaty/crypto/MainActivity.java
+++ app/src/main/java/ir/shariaty/crypto/MainActivity.java
@@ -1,14 +1,26 @@
 package ir.shariaty.crypto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText searchEdt;
+    private RecyclerView currenciesRV;
+    private ProgressBar loadingPB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchEdt = findViewById(R.id.idEdtSearch);
+        currenciesRV = findViewById(R.id.idRVCurrencies);
+        loadingPB = findViewById(R.id.idPBLoading);
+
     }
 }
