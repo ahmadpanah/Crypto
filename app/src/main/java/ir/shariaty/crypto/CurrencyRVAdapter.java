135b337cf8abba67206c50a1156425d08ff1a488
Hossein <djalicrt@gmail.com>
2021-12-13 12:29:27 +0330

    RecyclerView Logic almost Done. 📚

---
+++ app/src/main/java/ir/shariaty/crypto/CurrencyRVAdapter.java
@@ -0,0 +1,55 @@
+package ir.shariaty.crypto;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class CurrencyRVAdapter extends RecyclerView.Adapter<CurrencyRVAdapter.ViewHolder>{
+    private ArrayList<CurrencyRVModel> currencyRVModelArrayList;
+    private Context context;
+    private static DecimalFormat df2 = new DecimalFormat("#.##");
+
+    public CurrencyRVAdapter(ArrayList<CurrencyRVModel> currencyRVModelArrayList, Context context) {
+        this.currencyRVModelArrayList = currencyRVModelArrayList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CurrencyRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View view = LayoutInflater.from(context).inflate(R.layout.currency_rv_item,parent,false);
+       return new CurrencyRVAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CurrencyRVAdapter.ViewHolder holder, int position) {
+        CurrencyRVModel currencyRVModel = currencyRVModelArrayList.get(position);
+        holder.currencyNameTV.setText(currencyRVModel.getName());
+        holder.symbolTV.setText(currencyRVModel.getSymbol());
+        holder.rateTv.setText("$ " + df2.format(currencyRVModel.getPrice()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return currencyRVModelArrayList.size();
+    }
+
+    public class ViewHolder  extends RecyclerView.ViewHolder{
+        private TextView currencyNameTV, symbolTV, rateTv;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            currencyNameTV = itemView.findViewById(R.id.idTVCurrencyName);
+            symbolTV = itemView.findViewById(R.id.idTVSymbol);
+            rateTv = itemView.findViewById(R.id.idTVCurrencyRate);
+        }
+    }
+}
